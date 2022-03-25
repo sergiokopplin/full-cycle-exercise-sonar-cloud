0cec009988a6671806a15166e6bdec1a82f4f581
Sergio Kopplin <sergiokopplin@gmail.com>
2022-03-25 00:06:12 -0300

    fix: module exports on app

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ const sub = (num1, num2) => {
   return num1 - num2;
 }
 
-module.exports = sum;
-module.exports = sub;
+module.exports = {
+  sum, sub
+};
--- app.spec.js
+++ app.spec.js
@@ -1,15 +1,14 @@
-const sum = require('./app');
-const sub = require('./app');
+const app = require('./app');
 
 describe("Sum", () => {
   test("sum two numbers", () => {
-      expect(sum(1, 1)).toEqual(2);
+      expect(app.sum(1, 1)).toEqual(2);
   });
 });
 
 
 describe("Sub", () => {
   test("sub two numbers", () => {
-      expect(sub(1, 1)).toEqual(0);
+      expect(app.sub(1, 1)).toEqual(0);
   });
 });
