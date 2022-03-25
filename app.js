eb2de06fe8d5fc57f1d4ca7f015b517b6eb9e5b3
Sergio Kopplin <sergiokopplin@gmail.com>
2022-03-25 00:02:26 -0300

    feat: sub

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ const sum = (num1, num2) => {
   return num1 + num2;
 }
 
+const sub = (num1, num2) => {
+  return num1 - num2;
+}
+
 module.exports = sum;
+module.exports = sub;
--- app.spec.js
+++ app.spec.js
@@ -1,7 +1,15 @@
 const sum = require('./app');
+const sub = require('./app');
 
 describe("Sum", () => {
   test("sum two numbers", () => {
       expect(sum(1, 1)).toEqual(2);
+  });
+});
+
+
+describe("Sub", () => {
+  test("sub two numbers", () => {
+      expect(sub(1, 1)).toEqual(0);
   });
 });
